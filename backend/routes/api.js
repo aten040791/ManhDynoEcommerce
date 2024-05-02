062e589523acdfd2be0e4a7dcd2ee7c3a7ab42fb
manhdino <manhnguyen1238@gmail.com>
2024-05-02 18:46:16 +0700

    create post controller and test api index

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,21 +1,26 @@
-require('express-router-group');
-const express = require('express');
-const authController = require('modules/auth/controllers/authController');
-const categoriesController = require('modules/category/controllers/categoriesController');
-const router = express.Router({mergeParams: true})
+require("express-router-group");
+const express = require("express");
+const authController = require("modules/auth/controllers/authController");
+const categoriesController = require("modules/category/controllers/categoriesController");
+const postsController = require("modules/post/controllers/postsController");
+const router = express.Router({ mergeParams: true });
 
 //Single routing
 //Format: router.get(path, middlewareArray: optional = [], controllerAction)
-router.get('/helloworld', authController.helloWorld);
+router.get("/helloworld", authController.helloWorld);
 
-/** 
+/**
  * Nested routing
  * Format: router.group(path, middlewareArray: optional = [], (router) => {
  *      router.get(path, controllerAction)
  * })
  */
-router.group('/categories', (router) => {
-    router.get('/', categoriesController.index)
-})
+router.group("/categories", (router) => {
+  router.get("/", categoriesController.index);
+});
 
-module.exports = router
+router.group("/posts", (router) => {
+  router.get("/", postsController.index);
+});
+
+module.exports = router;
