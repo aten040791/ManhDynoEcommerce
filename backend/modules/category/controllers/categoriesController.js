02690e98dc1d2c76b7095fec92d1bb0d1189a8c9
aten040791 <aten040791@gmail.com>
2024-05-02 14:16:39 +0700

    Fix

--- backend/modules/category/controllers/categoriesController.js
+++ backend/modules/category/controllers/categoriesController.js
@@ -8,20 +8,10 @@ module.exports = {
 
     dummyCodeTestChatGPT: (req, res) => {
         const categories = "hihihi"
-<<<<<<< HEAD
-<<<<<<< HEAD
         return 'abcdef'
     },
 
     aSmartFn: (req, res) => {
         console.log('xxxx')
-=======
-
-        return 'abc'
->>>>>>> e9f0552 (Test chatgpt)
-=======
-
-        return 'abc'
->>>>>>> 07e5d1c (Test chatgpt)
     }
 }
