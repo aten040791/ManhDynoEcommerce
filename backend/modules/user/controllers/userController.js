e24b12bc4c22ce1b113db595a986b74513aac4d4
TienDuc <tienducnguyen174@gmail.com>
2024-05-03 17:33:46 +0700

    delete user

---
+++ backend/modules/user/controllers/userController.js
@@ -0,0 +1,38 @@
+const rs = require('services/response');
+const userService = require('../services/userService');
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await userService.getAllUsersService();
+        if (users) {
+            return rs.ok(res, {
+                users
+            })
+        } else {
+            return rs.notFound()
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const id = req.query.id;
+        if (id) {
+            await userService.deleteUserService(id);
+            return rs.ok(res, {
+                mes: "Deleted user successfully"
+            });
+        } else {
+            return rs.notFound();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    deleteUser
+}
