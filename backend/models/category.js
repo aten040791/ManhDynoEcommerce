6e6cb72e3e3cc914bbd3c740b076f04ea5385f6f
TienDuc <tienducnguyen174@gmail.com>
2024-05-04 10:38:49 +0700

    crud category

---
+++ backend/models/category.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    // static associate(models) {
+    //   Category.hasMany(models.Post, {
+    //     foreignKey: "category_id",
+    //     as: "post",
+    //   });
+    // }
+  }
+  Category.init(
+    {
+      name: {
+        type: DataTypes.STRING(20),
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Category",
+      timestamps: false,
+    }
+  );
+  return Category;
+};
