a36a0631090ec4a74fa8630320674ef552d32572
manhdino <manhnguyen1238@gmail.com>
2024-05-07 11:37:57 +0700

    fix route

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,30 +3,38 @@ const express = require("express");
 const authController = require("modules/auth/controllers/authController");
 const postsController = require("modules/post/controllers/postsController");
 const languagesController = require("modules/languages/controllers/languageController");
-const { user, owner } = require("../middlewares/authMiddleware");
+const { user, owner, admin } = require("../middlewares/authMiddleware");
 const router = express.Router({ mergeParams: true });
 
-router.group("/api/auth", (router) => {
+router.group("/auth", (router) => {
   router.post("/sign-in", authController.signIn);
   router.post("/sign-up", authController.signUp);
   router.get("/recover-password", authController.recoverPassword);
   router.put("/reset-password", authController.resetPassword);
 });
 
-router.group("/languages", (router) => {
+router.group("/languages", user, (router) => {
   router.get("/", languagesController.index);
   router.get("/:languageId", languagesController.show);
-  router.post("/create", languagesController.create);
-  router.put("/update/:languageId", languagesController.update);
-  router.delete("/delete/:languageId", languagesController.destroy);
 });
 
-router.group("/api/posts", user, (router) => {
+router.group("/languages", owner, (router) => {
+  router.post("/create", postsController.create);
+  router.put("/update/:postId", postsController.update);
+  router.delete("/delete/:postId", postsController.destroy);
+});
+
+// router.group("/users", admin, (router) => {
+//   router.get("/", userController.index);
+//   router.delete("/", userController.destroy);
+// });
+
+router.group("/posts", user, (router) => {
   router.get("/", postsController.index);
   router.get("/:postId", postsController.show);
 });
 
-router.group("/api/posts", owner, (router) => {
+router.group("/posts", owner, (router) => {
   router.post("/create", postsController.create);
   router.put("/update/:postId", postsController.update);
   router.delete("/delete/:postId", postsController.destroy);
