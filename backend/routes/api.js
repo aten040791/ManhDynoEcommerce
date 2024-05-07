3afc8789f996aa2f4d90c31588110eb17b15dc3a
manhdino <manhnguyen1238@gmail.com>
2024-05-07 14:35:41 +0700

    fix route

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -9,27 +9,6 @@ router.group("/auth", (router) => {
   router.post("/sign-in", authController.signIn);
 });
 
-// router.group("/languages", user, (router) => {
-//   router.get("/", languagesController.index);
-//   router.get("/:languageId", languagesController.show);
-// });
-
-// router.group("/languages", owner, (router) => {
-//   router.post("/create", postsController.create);
-//   router.put("/update/:postId", postsController.update);
-//   router.delete("/delete/:postId", postsController.destroy);
-// });
-
-// router.group("/categories", user, (router) => {
-//   router.get("/", categoriesController.index);
-// });
-
-// router.group("/categories", owner, (router) => {
-//   router.post("/create", categoriesController.create);
-//   router.put("/update/:id", categoriesController.update);
-//   router.delete("/delete/:id", categoriesController.destroy);
-// });
-
 router.group("/posts", user, (router) => {
   router.get("/", postsController.index);
   router.get("/:postId", postsController.show);
