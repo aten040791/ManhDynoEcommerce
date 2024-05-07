b8e79d1ce486a92a3c88ad6d5899b7287c8582e4
nguyenkyky <scholesyy30@gmail.com>
2024-05-07 22:08:40 +0700

    sua validate

--- backend/validations/languageValidation.js
+++ backend/validations/languageValidation.js
@@ -14,11 +14,15 @@ module.exports = {
     const schema = Joi.object({
       name: Joi.string().max(20).required(),
       locale: Joi.string()
-        .pattern(/^[a-z_]$/)
-        .required()
+        .regex(/^[a-z_]+$/)
+        .min(2)
+        .max(10)
+        .default("en_us")
         .messages({
           "string.pattern.base":
             "The language locale contains only the characters a-z and _",
+          "string.min": "Language locale must have at least 2 characters",
+          "string.max": "Language locale must have at most 10 characters",
           "any.required": "Language locale is required",
         }),
       flag: Joi.string().required(),
@@ -33,12 +37,16 @@ module.exports = {
       languageId: Joi.number().required(),
       name: Joi.string().max(20),
       locale: Joi.string()
-        .pattern(/^[a-z_]$/)
+        .regex(/^[a-z_]+$/)
         .min(2)
         .max(10)
+        .default("en_us")
         .messages({
           "string.pattern.base":
             "The language locale contains only the characters a-z and _",
+          "string.min": "Language locale must have at least 2 characters",
+          "string.max": "Language locale must have at most 10 characters",
+          "any.required": "Language locale is required",
         }),
       flag: Joi.string(),
     });
