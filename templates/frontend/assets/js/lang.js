10d423dc21e04a2f9d864fcb5243049dc8b8c42d
manhdino <manhnguyen1238@gmail.com>
2024-05-08 10:58:30 +0700

    add change language

--- templates/frontend/assets/js/lang.js
+++ templates/frontend/assets/js/lang.js
@@ -1,7 +1,15 @@
+
+import en from "../data/en.json" with { type: "json" };
+import vi from "../data/vi.json" with { type: "json" };
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const iconBars = document.querySelector("#icon-bars");
   const headerNav = document.querySelector("#header-nav");
+  const headerNavItems = document.querySelectorAll("#header-nav .header-nav-item");
   const langDropdown = document.querySelector("#lang-dropdown");
+  const textsLangChange = document.querySelectorAll("[data-section]");
+  let language = "English";
   iconBars.addEventListener("click", function () {
     if (langDropdown.classList.contains("show")) {
       langDropdown.classList.remove("show");
@@ -14,16 +22,30 @@ document.addEventListener("DOMContentLoaded", function () {
     this.classList.add("show");
   });
 
+
+
   const dropdownItems = document.querySelectorAll(".dropdown-item");
   dropdownItems.forEach((item) => {
     item.addEventListener("click", function (event) {
       event.preventDefault();
-      const language = this.textContent.trim();
+       language = this.textContent.trim();
       const flagPath = `../../../backend/owner/assets/images/flags/${
         language === "English" ? "US" : "VN"
       }.png`;
 
-      langDropdown.innerHTML = `<img src="${flagPath}" class="w-40" alt="${language}"> <a href="#" class="text-decoration-none color-disabled fw-600">${language}</a>`;
+      langDropdown.innerHTML = `<img src="${flagPath}" class="w-40" alt="${language}"> <a href="#" class="text-decoration-none color-disabled fw-600">${language}</a>`; 
+      textsLangChange.forEach((item) =>{
+        const section = item.dataset.section;
+        const value = item.dataset.value;
+        if(language === "English"){
+            item.textContent = en[section][value];
+        }else{
+            item.textContent = vi[section][value];
+        }
+      })
     });
   });
+
+
 });
+
