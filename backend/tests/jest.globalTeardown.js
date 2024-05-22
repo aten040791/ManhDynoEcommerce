18685af8dea88c2e1f323da47f826d515933f65d
aten040791 <46781005+aten040791@users.noreply.github.com>
2024-05-22 17:41:41 +0700

    Add test (#76)

--- backend/tests/jest.globalTeardown.js
+++ backend/tests/jest.globalTeardown.js
@@ -1,8 +1,10 @@
 module.exports = async () => {
     const sequelize = global.__SEQUELIZE__;
+    const umzug = global.__UMZUG__;
 
     if (sequelize) {
         try {
+            await umzug.down({to: 0})
             await sequelize.close();
             console.log('Global teardown: Connection has been closed successfully.');
         } catch (error) {
