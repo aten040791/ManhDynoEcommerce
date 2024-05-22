0d9174d3504196d031f1442e300b9414f9923e89
aten040791 <46781005+aten040791@users.noreply.github.com>
2024-05-22 10:27:38 +0700

    Enhancements (#68)
    * Enhancements
    
    * Enhancements

---
+++ backend/kernels/rules/param.js
@@ -0,0 +1,12 @@
+const { param } = require("express-validator");
+const {BodyWithLocale} = require("kernels/rules");
+
+class ParamsWithLocale extends BodyWithLocale 
+{
+    constructor(field) {
+        super(field)
+        this.field = param(field)
+    }
+}
+
+module.exports = ParamsWithLocale
